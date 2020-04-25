979001c6972b4c249c928f911b4537fe6de29012
Sidney Simmons <sas.simm@gmail.com>
2020-04-25 15:31:13 -0600

    #1 Implement recurring method to choose random background color

---
+++ src/main/java/com/sidneysimmons/simple/web/socket/configuration/SchedulingConfiguration.java
@@ -0,0 +1,15 @@
+package com.sidneysimmons.simple.web.socket.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Scheduling configuration.
+ * 
+ * @author Sidney Simmons
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfiguration {
+
+}
