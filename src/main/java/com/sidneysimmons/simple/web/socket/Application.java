b1733402097858f70f81066986f254c76c3c93f6
Sidney Simmons <sas.simm@gmail.com>
2020-04-25 14:42:16 -0600

    #1 Create initial spring boot skeleton

---
+++ src/main/java/com/sidneysimmons/simple/web/socket/Application.java
@@ -0,0 +1,18 @@
+package com.sidneysimmons.simple.web.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point to the application!
+ * 
+ * @author Sidney Simmons
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
