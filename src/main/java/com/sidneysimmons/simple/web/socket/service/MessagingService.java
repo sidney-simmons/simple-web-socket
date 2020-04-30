31f08f08cc13b8086295e721dedfaf6093c7b633
Sidney Simmons <sas.simm@gmail.com>
2020-04-29 18:48:59 -0600

    #1 Allow disabling/enabling the system broadcast messages

--- src/main/java/com/sidneysimmons/simple/web/socket/service/MessagingService.java
+++ src/main/java/com/sidneysimmons/simple/web/socket/service/MessagingService.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +15,19 @@ public class MessagingService {
     @Resource(name = "webSocketService")
     private WebSocketService webSocketService;
 
+    @Value("${system.broadcast.message.enabled}")
+    private Boolean systemBroadcastEnabled;
+
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss zzz")
             .withZone(ZoneId.systemDefault());
 
     @Scheduled(fixedDelayString = "${system.broadcast.message.delay}")
     private void sendSystemBroadcastMessage() {
-        BroadcastMessage message = new BroadcastMessage();
-        message.setMessage("System broadcast on " + DATE_TIME_FORMATTER.format(Instant.now()) + ".");
-        webSocketService.sendMessage(message);
+        if (systemBroadcastEnabled) {
+            BroadcastMessage message = new BroadcastMessage();
+            message.setMessage("System broadcast on " + DATE_TIME_FORMATTER.format(Instant.now()) + ".");
+            webSocketService.sendMessage(message);
+        }
     }
 
 }
